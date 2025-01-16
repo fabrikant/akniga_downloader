482938d371a153227615c4f5ffec27c97bb1f5da
Artem <banderovaa@gmail.com>
2025-01-16 14:04:01 +0600

    Загрузка общех модулей перенесена в install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 DIR=$(dirname $0)
 cd $DIR
+
+#Загрузка общих для всех качалок модулей
+rm -rf opf.py tg_sender.py common_arguments.py
+wget https://raw.githubusercontent.com/fabrikant/litres_audiobooks_downloader/main/opf.py
+wget https://raw.githubusercontent.com/fabrikant/litres_audiobooks_downloader/main/tg_sender.py
+wget https://raw.githubusercontent.com/fabrikant/litres_audiobooks_downloader/main/common_arguments.py
+
 ./download_external_modules.sh
 python3 -m venv .venv
 source .venv/bin/activate
